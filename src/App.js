862040b12e3d7a9ab7d11014f8073647be6e6d8d
bilcko <iubilcko@gmail.com>
2022-07-17 22:32:03 +0300

    web socket on a fingers

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import SettingBar from "./components/SettingBar";
 import Canvas from "./components/Canvas";
 
 function App() {
+    const socket = new WebSocket('ws://localhost:5000/')
+    socket.onopen = () => {
+        socket.send(JSON.stringify({
+            method: 'connection',
+            id: 555,
+            username: 'vlad'
+        }))
+    }
+    socket.onmessage = (event) => {
+        console.log('С сервера пришло сообшение:', event.data)
+    }
+    const sendMassage = () => {
+        socket.send(JSON.stringify({
+            message: "Привет",
+            method: 'message',
+            id: Date.now(),
+            username: 'vlad'
+        }))
+    }
   return (
     <div className="App">
+        <button onClick={() => sendMassage()}>111111111111</button>
       <Toolbar></Toolbar>
       <SettingBar></SettingBar>
       <Canvas></Canvas>
